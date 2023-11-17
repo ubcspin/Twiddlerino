8a4b101c06e2b80e5d45e40ec75ff8a776f2aa38
Bereket Guta <bguta@hadron.local>
2023-11-17 11:39:20 -0800

    add hardware documents and library files

---
+++ lib/Twiddlerino.h
@@ -0,0 +1,17 @@
+#ifndef Twiddlerino_h
+#define Twiddlerino_h
+
+//initializes Twiddlerino
+void TwiddlerinoInit();
+
+//Reads the encoder returning a signed integer value
+int ReadEncoder();
+
+//Resets the encoder to zero
+void ZeroEncoder();
+
+//Sets the output to the motor (between -255 and 255)
+void SetPWMOut(int);
+
+#endif
+
